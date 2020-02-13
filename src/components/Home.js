6eafcf8eb0a52618287af4439250333c85cb20e1
Moses Ogwo <mosesogwo@gmail.com>
2020-02-13 15:05:33 +0100

    import createStore, Provider in index.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, NavLink } from 'react-router-dom'
 
 class Home extends Component {
 
-
   render = () => {
     return(
-      <div>
-        <header>
-          <div className='logo-div'>
-            <h1>Xplore!</h1>
-          </div>
-          <nav>
-
-          </nav>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <header>
+            <div className='logo-div'>
+              <h1>Xplore!</h1>
+            </div>
+            <nav>
+              <ul>
+                <li><NavLink to="/login">Login</NavLink></li>
+                <li><NavLink to="/packages">Packages</NavLink></li>
+                <li><NavLink to="/wishlist">My WishList</NavLink></li>
+  
+              </ul>
+            </nav>
+          </header>
+        </div>
+      </Router>
     )
   }
 }
---
+++ src/components/Packages.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Packages extends Component {
+
+  render = () => {
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
+
