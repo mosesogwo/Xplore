f54197974f1bbe68afd480f6b0417da5de3f1c0f
Moses Ogwo <mosesogwo@gmail.com>
2020-02-13 14:44:26 +0100

    install react-router-dom

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom'
+
+class Home extends Component {
+
+
+  render = () => {
+    return(
+      <div>
+        <header>
+          <div className='logo-div'>
+            <h1>Xplore!</h1>
+          </div>
+          <nav>
+
+          </nav>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default Home;
