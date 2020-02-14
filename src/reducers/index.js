2de0d4830ac8a13b7714c30cdd4986d99bf23e76
Moses Ogwo <mosesogwo@gmail.com>
2020-02-14 22:08:11 +0100

    add wishlist reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import usernameReducer from './usernameReducer';
 import packagesReducer from './packagesReducer';
+import wishlistReducer from './wishlistReducer';
 
 const rootReducer = combineReducers({
   username: usernameReducer,
-  packages: packagesReducer
+  packages: packagesReducer,
+  wishlist: wishlistReducer,
 });
 
 export default rootReducer;
