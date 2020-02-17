333a079ee29092cc76afe6d38fab95d880c1d1fd
Moses Ogwo <mosesogwo@gmail.com>
2020-02-17 08:05:07 +0100

    Fix some ESLint errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,19 +7,21 @@ const Home = props => {
   const { username } = props;
 
   return (
-    <div>
-      <header>
-        <div className="logo-div">
-          <h1>Xplore!</h1>
+    <div className="home">
+      <header className="home-header">
+        <div className="header-div">
+          <div className="logo-div">
+            <h1>Xplore!</h1>
+          </div>
+          <nav>
+            <ul>
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/login">{username !== '' ? 'Logout' : 'Login'}</NavLink></li>
+              <li><NavLink to="/packages">Packages</NavLink></li>
+              <li><NavLink to="/wishlist">{ username !== '' ? 'My Wishlist' : '' }</NavLink></li>
+            </ul>
+          </nav>
         </div>
-        <nav>
-          <ul>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/login">{username !== '' ? 'Logout' : 'Login'}</NavLink></li>
-            <li><NavLink to="/packages">Packages</NavLink></li>
-            <li><NavLink to="/wishlist">{ username !== '' ? 'My Wishlist' : '' }</NavLink></li>
-          </ul>
-        </nav>
       </header>
     </div>
   );
